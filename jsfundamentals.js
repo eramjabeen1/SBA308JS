3755c061874961b758cec87bb9e8a07e929646a1
Eram Jabeen <contacteramjabeen@gmail.com>
2025-03-06 15:16:29 -0500

    SBA 308 JS Fundamentals setup

---
+++ jsfundamentals.js
@@ -0,0 +1,13 @@
+//SBA 308 javascript fundamentals
+//creating a grade processing app to process and manage scores of students
+//also to calculate scores and averages
+//objective here to 
+//assessment for js fundamentals using basic js syntax..
+//implementing ctrl flow structures conditionals and loops
+//arrays and objects for data
+//functions for reusable code
+//loops and iteration for navigation thru data collections
+//implementing error handling for any code failures
+//revise methods of recursions and any topic needed for this SBA along the way
+
+//function setup to see if assignments groups are in the correct course
