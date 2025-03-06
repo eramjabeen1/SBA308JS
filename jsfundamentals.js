d71a62b1410938f0b8766838904c5b33b170bb8a
Eram Jabeen <contacteramjabeen@gmail.com>
2025-03-06 15:37:29 -0500

    adding jsfundamentals js file finally as i was lost before and sent it to wrong repo

--- jsfundamentals.js
+++ jsfundamentals.js
@@ -11,3 +11,21 @@
 //revise methods of recursions and any topic needed for this SBA along the way
 
 //function setup to see if assignments groups are in the correct course
+function checkCourse(assignmentGroups, course) {
+    assignmentGroups.forEach(group => {
+        if (group.course_id !== course.id) {
+            throw new Error("error: assignment group " + group.id + " does not belong to course " + course.id);
+        }
+    });
+}
+
+// function to remove assignments that are not yet due
+function filterDueAssignments(assignmentGroups) {
+    const today = new Date();
+
+    return assignmentGroups.map(group => {
+        let dueAssignments = group.assignments.filter(assignment => new Date(assignment.due_at) <= today);
+
+        return { id: group.id, assignments: dueAssignments };
+    }).filter(group => group.assignments.length > 0);
+}
